47f24fcc019b2d70d7eac04135a3f92a1a90d229
Logan <tkdkop@hotmail.com>
2014-11-10 17:34:06 -0600

    create canvas start

---
+++ index.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Logan on 2014-11-10.
+ */
+
+function createWindow(canvas){
+    var ctx = canvas.getContext("2d");
+    ctx.font = "50px Arial";
+    ctx.strokeText("Hey There", 50, 50);
+}
