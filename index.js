101d00752159deccb514f9c9c9e98f696d3675af
Jesse Rolheiser <jlr297@gmail.com>
2014-11-10 21:14:55 -0600

    Added the scaffolding for server/client communication.

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
 /**
  * Created by Logan on 2014-11-10.
  */
+var ctx;
+var socket = io();
 
 function createWindow(canvas){
-    var ctx = canvas.getContext("2d");
-    ctx.font = "50px Arial";
-    ctx.strokeText("Hey There", 50, 50);
+	document.getElementById('messenger').style.backgroundColor = "#aaaaaa";
+	canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    
+    ctx = canvas.getContext("2d");
+    ctx.font = "20px Arial";
+}
+
+socket.on('whateverServerMessege', onBlahBlah);
+socket.on('newUser', onNewUser);
+socket.on('userLeft', onUserLeft);
+
+function onBlahBlah(dataFromServer){
+	ctx.fillText(dataFromServer.messege + " from " + dataFromServer.name, 50, 100);
+}
+
+function onNewUser(data){
+	ctx.fillText("Someone new has joined!", 50, 150);
+}
+
+function onUserLeft(data){
+	ctx.fillText("Someone new has joined!", 50, 200);
 }
